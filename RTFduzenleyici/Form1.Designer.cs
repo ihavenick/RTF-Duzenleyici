771a8e2b2809efcd5550201d953c507c5f008e35
Ata Çetin <ihavenick@gmail.com>
2021-02-27 00:48:11 +0300

    inital commit

---
+++ RTFduzenleyici/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace RTFduzenleyici
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dosyaSec = new System.Windows.Forms.OpenFileDialog();
+            this.btnDosyaSec = new System.Windows.Forms.Button();
+            this.btnCevrilmisDosyaSec = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtCevrilcekYol = new System.Windows.Forms.TextBox();
+            this.txtCevrilmisDosya = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dosyaKaydet = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // dosyaSec
+            // 
+            this.dosyaSec.FileName = "secilenDosya";
+            this.dosyaSec.FileOk += new System.ComponentModel.CancelEventHandler(this.dosyaSec_FileOk);
+            // 
+            // btnDosyaSec
+            // 
+            this.btnDosyaSec.Location = new System.Drawing.Point(280, 60);
+            this.btnDosyaSec.Name = "btnDosyaSec";
+            this.btnDosyaSec.Size = new System.Drawing.Size(84, 23);
+            this.btnDosyaSec.TabIndex = 0;
+            this.btnDosyaSec.Text = "Seç";
+            this.btnDosyaSec.UseVisualStyleBackColor = true;
+            this.btnDosyaSec.Click += new System.EventHandler(this.btnDosyaSec_Click);
+            // 
+            // btnCevrilmisDosyaSec
+            // 
+            this.btnCevrilmisDosyaSec.Location = new System.Drawing.Point(280, 89);
+            this.btnCevrilmisDosyaSec.Name = "btnCevrilmisDosyaSec";
+            this.btnCevrilmisDosyaSec.Size = new System.Drawing.Size(84, 23);
+            this.btnCevrilmisDosyaSec.TabIndex = 1;
+            this.btnCevrilmisDosyaSec.Text = "Seç";
+            this.btnCevrilmisDosyaSec.UseVisualStyleBackColor = true;
+            this.btnCevrilmisDosyaSec.Click += new System.EventHandler(this.btnCevrilmisDosyaSec_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(151, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtCevrilcekYol
+            // 
+            this.txtCevrilcekYol.Location = new System.Drawing.Point(103, 60);
+            this.txtCevrilcekYol.Name = "txtCevrilcekYol";
+            this.txtCevrilcekYol.Size = new System.Drawing.Size(170, 20);
+            this.txtCevrilcekYol.TabIndex = 3;
+            // 
+            // txtCevrilmisDosya
+            // 
+            this.txtCevrilmisDosya.Location = new System.Drawing.Point(103, 91);
+            this.txtCevrilmisDosya.Name = "txtCevrilmisDosya";
+            this.txtCevrilmisDosya.Size = new System.Drawing.Size(170, 20);
+            this.txtCevrilmisDosya.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cevrilcek Dosya:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cevrilmiş Dosya:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 306);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCevrilmisDosya);
+            this.Controls.Add(this.txtCevrilcekYol);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnCevrilmisDosyaSec);
+            this.Controls.Add(this.btnDosyaSec);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(410, 345);
+            this.MinimumSize = new System.Drawing.Size(410, 345);
+            this.Name = "Form1";
+            this.Text = "Docx Duzenleyici";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog dosyaSec;
+        private System.Windows.Forms.Button btnDosyaSec;
+        private System.Windows.Forms.Button btnCevrilmisDosyaSec;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtCevrilcekYol;
+        private System.Windows.Forms.TextBox txtCevrilmisDosya;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog dosyaKaydet;
+    }
+}
+
---
+++ RTFduzenleyici/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Xceed.Words.NET;
+using System.Text.RegularExpressions;
+
+namespace RTFduzenleyici
+{
+    public partial class Form1 : Form
+    {
+        private string DosyaAdi;
+        private string DosyaYolu;
+        private string kaydedilenDosya;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dosyaSec_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnDosyaSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = dosyaSec;
+            file.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            file.Filter = "Word Dosyası |*.docx";
+            file.FilterIndex = 1;
+            file.Title = "Word Dosyası Seçiniz..";
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = file.FileName;
+                DosyaAdi = file.SafeFileName;
+                txtCevrilcekYol.Text = DosyaYolu;
+            }
+        }
+
+        private void btnCevrilmisDosyaSec_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = dosyaKaydet;
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveFileDialog.Title = "Dosyayı nereye kaydetmek istersiniz?";
+            saveFileDialog.Filter = "Word Dosyası |*.docx";
+            saveFileDialog.FilterIndex = 1;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                kaydedilenDosya = saveFileDialog.FileName;
+                txtCevrilmisDosya.Text = kaydedilenDosya;
+            }
+        }
+
+        public string tirnakCevirici(string gelen)
+        {
+            string pattern = @"[']+\w+[']";
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(gelen, pattern, options))
+            {
+                string temp = m.Value;
+                temp = temp.Substring(1, temp.Length - 2);
+                temp = @"""" + temp + "\"";
+
+                return gelen.Replace(m.Value, temp);
+            }
+
+            return gelen;
+        }
+
+        public string saatCevirici(string gelen)
+        {
+            string pattern = @"\d+\d+[:]+\d+\d";
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(gelen, pattern, options))
+            {
+                string temp = "";
+                string[] cumleler = gelen.Split(':');
+                foreach (var cumle in cumleler)
+                {
+                    if (temp.Length == 0)
+                    {
+                        temp += cumleler[0];
+                    }
+                    else
+                    {
+
+                        temp = temp + "-" + cumle;
+                    }
+                }
+                return temp;
+            }
+
+            return gelen;
+        }
+
+        public string sayiCevirici(string gelen)
+        {
+            string pattern = @"\d+[.]+\d";
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(gelen, pattern, options))
+            {
+                string temp = "";
+                string[] cumleler = gelen.Split('.');
+                foreach (var cumle in cumleler)
+                {
+                    temp += cumle;
+                }
+                return temp;
+            }
+
+            return gelen;
+        }
+
+        public string satirsonuCizgiAlgilayici(string gelen)
+        {
+            string pattern = @"\w+[-]";
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(gelen, pattern, options))
+            {
+                string temp = "";
+                temp = gelen.TrimEnd('-');
+
+                return temp;
+            }
+
+            return gelen;
+        }
+
+
+        private static Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+        public static int RomanToInteger(string roman)
+        {
+            int number = 0;
+            char previousChar = roman[0];
+            foreach (char currentChar in roman)
+            {
+                number += RomanMap[currentChar];
+                if (RomanMap[previousChar] < RomanMap[currentChar])
+                {
+                    number -= RomanMap[previousChar] * 2;
+                }
+                previousChar = currentChar;
+            }
+            return number;
+        }
+
+        public string romenRakamCevirici(string gelen)
+        {
+            string pattern = @"^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$";
+            RegexOptions options = RegexOptions.Multiline;
+
+            string[] cumleler = gelen.Split(' ');
+            string temp = "";
+            bool degisti = false;
+            bool yazma = false;
+            foreach (var cumle in cumleler)
+            {
+                if (cumle.Length > 0)
+                {
+                    yazma = false;
+                    foreach (Match m in Regex.Matches(cumle, pattern, options))
+                    {
+                        int cevrilen = RomanToInteger(cumle);
+                        degisti = true;
+                        yazma = true;
+                        temp += " " + cevrilen.ToString();
+                    }
+                    if (!yazma)
+                    {
+                        temp += " " + cumle;
+                    }
+
+                }
+            }
+            if (degisti)
+            {
+                return temp;
+            }
+            return gelen;
+        }
+
+        public string satirbasiAlintiCevirici(string gelen)
+        {
+            string pattern = @"(^|\s)[-]+\w";
+            RegexOptions options = RegexOptions.Multiline;
+
+
+            foreach (Match m in Regex.Matches(gelen, pattern, options))
+            {
+                string temp = "";
+                temp += "-" + gelen;
+                return temp;
+
+            }
+
+            return gelen;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (DosyaYolu.Length != 0 && kaydedilenDosya.Length != 0)
+            {
+
+                using (DocX acilanDosya = DocX.Load(DosyaYolu))
+                {
+
+                    foreach (Paragraph p in acilanDosya.Paragraphs)
+                    {
+                        string temp = p.Text;
+                        string temp2 = "  ";
+                        int birparagraftakiAltiCizililer = 0;
+                        foreach (var item in p.MagicText)
+                        {
+                            if (item.formatting != null)
+                            {
+                                
+
+                                if (item.formatting.UnderlineStyle == UnderlineStyle.singleLine)
+                                {
+                                    birparagraftakiAltiCizililer += 1;
+
+                                    string[] cumleler = item.text.Split(' ');
+
+                                    foreach (var cumle in cumleler)
+                                    {
+                                        temp2 += " (i) " + cumle;
+                                    }
+
+
+                                    //temp2 += " (i)" + item.text;
+                                }
+                                else
+                                {
+                                    temp2 += item.text;
+                                }
+                            }
+                            else
+                            {
+                                temp2 += item.text;
+                            }
+                        }
+
+
+                        p.ReplaceText(temp, temp2);
+
+                        p.ReplaceText(temp, tirnakCevirici(temp));
+                        p.ReplaceText(temp, sayiCevirici(temp));
+                        p.ReplaceText(temp, satirsonuCizgiAlgilayici(temp));
+                        p.ReplaceText(temp, satirbasiAlintiCevirici(temp));
+                        p.ReplaceText(temp, romenRakamCevirici(temp));
+                        p.ReplaceText(temp, saatCevirici(temp));
+                    }
+
+                    acilanDosya.ReplaceText("- -", "--");
+                    acilanDosya.SaveAs(kaydedilenDosya);
+                    MessageBox.Show("Kayıt başarılı.");
+                }
+            }
+        }
+    }
+}
